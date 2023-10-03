579801bc669ca113cb4f145b8bdc241ea855b35e
siddhi1102 <siddhirajoria02@gmail.com>
2023-10-03 16:58:29 +0530

    Add files via upload

---
+++ Siddhi Rajoria/src/app/Models/get-loc.ts
@@ -0,0 +1,16 @@
+export interface GetLoc {
+    boundingbox : string [],
+    class : string,
+    display_name : string,
+    importance : number,
+    lat : string,
+    lon : string,
+    licence : string,
+    osm_id : number,
+    osm_type : string,
+    place_id : number,
+    powered_by : string,
+    type : string
+}
+
+
