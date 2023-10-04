f2f60713d4a743394a17316b8645ce206043afc7
Thrupthi <107300836+Thrupthi-G@users.noreply.github.com>
2023-10-04 17:29:01 +0530

    Delete Thrupthi G/Pipe directory

--- Thrupthi G/Pipe/city-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CityFilterPipe } from './city-filter.pipe';
-
-describe('CityFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CityFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
